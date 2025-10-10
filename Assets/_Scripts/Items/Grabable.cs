ebaffdd974af89b9ac5e9a73393408fba59d3c7c
Miguel Lazaro Gil <lazaro.gil.miguel@gmail.com>
2025-10-10 14:05:05 +0200

    Coger ya no mete colisiones player objeto

--- Assets/_Scripts/Items/Grabable.cs
+++ Assets/_Scripts/Items/Grabable.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Events;
 [RequireComponent(typeof(ParentConstraint))]
+[RequireComponent(typeof(Rigidbody))]
 public class Grabable : MonoBehaviour
 {
 
@@ -10,10 +11,14 @@ public class Grabable : MonoBehaviour
     public UnityEvent OnGrab;
     public UnityEvent OnDrop;
     public bool canBeGrabbed = true;
+    private Rigidbody rb;
+    private int originalLayer;
     private void Awake()
     {
         parentConstraint = GetComponent<ParentConstraint>();
         parentConstraint.constraintActive = false;
+        rb = GetComponent<Rigidbody>();
+        originalLayer = gameObject.layer;
     }
     public virtual bool Grab(Transform grabbingTransform)
     {
@@ -22,9 +27,10 @@ public class Grabable : MonoBehaviour
 
         Debug.Log("Grabable.Grab()");
 
+        
+        rb.excludeLayers = LayerMask.GetMask("Player");
+        gameObject.layer = LayerMask.NameToLayer("GrabbedObj");
         transform.position = grabbingTransform.position;
-       // transform.rotation = grabbingTransform.rotation;
-
         parentConstraint.AddSource( new ConstraintSource() { sourceTransform = grabbingTransform, weight = 1 });
         parentConstraint.constraintActive = true;
         OnGrab?.Invoke();
@@ -35,6 +41,8 @@ public class Grabable : MonoBehaviour
         Debug.Log("Grabable.Drop()");
         parentConstraint.RemoveSource(0);
         parentConstraint.constraintActive = false;
+        rb.excludeLayers = 0;
+        gameObject.layer = originalLayer;
         OnDrop?.Invoke();
 
         return true; //el objeto se ha soltado
