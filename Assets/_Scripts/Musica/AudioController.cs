83df44b0924c2fd7f381c9e7f0272d346015db10
Albi52 <pozvel@gmail.com>
2025-10-17 20:17:43 +0200

    MenusPrefabs y funcionalidad del Menu de Opciones

--- Assets/_Scripts/Musica/AudioController.cs
+++ Assets/_Scripts/Musica/AudioController.cs
@@ -5,8 +5,16 @@ public class AudioController : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
 
+    private void Start()
+    {
+        float volume = Settings.Instance.GetVolume();
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+
     public void ChangeVolume(float volume)
     {
+        if(Settings.Instance.IsMuted()) return;
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+        Settings.Instance.NewVolume(volume);
     }
 }
