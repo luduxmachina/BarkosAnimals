9eb890fd21c8989648fe2121e78d994256af1d96
Miguel Lazaro Gil <lazaro.gil.miguel@gmail.com>
2025-10-17 21:11:13 +0200

    Solucionado bug de varios gamemanagers

--- Assets/_Scripts/SceneManaging/GameFlowManager.cs
+++ Assets/_Scripts/SceneManaging/GameFlowManager.cs
@@ -15,6 +15,7 @@ public enum GameModes //esto es criminal y está hecho con spaghetti, todo lo que
 }
 public class GameFlowManager : MonoBehaviour
 {
+    public static GameFlowManager instance;
     [Header("Levels to play")]
     [SerializeField]
     private NivelSO defaultLevel;
@@ -286,6 +287,11 @@ public class GameFlowManager : MonoBehaviour
 
     private void Awake()
     {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
         DontDestroyOnLoad(this.gameObject);
 
     }
