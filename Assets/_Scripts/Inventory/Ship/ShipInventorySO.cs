ef8a53b925d5351c6412698ff77fd8dc129b8e09
FRANCISCO PEÑA CALDITO <125310916+robotronic12@users.noreply.github.com>
2025-10-19 19:35:41 +0200

    Debug de inventario entre escenas

--- Assets/_Scripts/Inventory/Ship/ShipInventorySO.cs
+++ Assets/_Scripts/Inventory/Ship/ShipInventorySO.cs
@@ -9,6 +9,16 @@ public class ShipInventorySO : ScriptableObject
     private AllObjectTypesSO dataBase;
     private readonly Dictionary<ItemNames, int> inventory = new();
     
+    [ContextMenu("Debug Inventory")]
+    public void DebugMensaje()
+    {
+        Debug.Log($"[ShipInventorySO]:");
+        foreach (var kvp in inventory)
+        {
+            Debug.Log($"\t{kvp.Key} => {kvp.Value}");
+        }
+    }
+    
 
     public List<InventoryItemDataObjects> GetAllStacksWithItemName(ItemNames itemName)
     {
