fa9fc473101f68543e879670896e2214f88272a7
robotronic12 <fran120304@gmail.com>
2025-10-30 15:53:18 +0100

    Funciones de testeo del guardado de información de los objetos del mundo

--- Assets/_Scripts/Grid/WorldGen/TestWorldGen.cs
+++ Assets/_Scripts/Grid/WorldGen/TestWorldGen.cs
@@ -9,7 +9,49 @@ public class TestWorldGen : MonoBehaviour
     {
         foreach(var obj in database.worldObjectData)
         {
-            Debug.Log($"la matriz es de {obj.B_OcupiedSpace.GetColums()} x {obj.B_OcupiedSpace.GetRows()}");
+            Debug.Log($"La matriz [{obj.Name}] es de {obj.B_OcupiedSpace.GetRows()} x {obj.B_OcupiedSpace.GetColums()}");
+            
+            Debug.Log($"\t Matriz B:");
+            for (int i = 0; i < obj.B_OcupiedSpace.GetRows(); i++)
+            {
+                string str = "\t";
+                for (int j = 0; j < obj.B_OcupiedSpace.GetColums(); j++)
+                {
+                    if (obj.B_OcupiedSpace.GetValue(i, j)) 
+                    {
+                        str += "X";
+                    }
+                    else
+                    {
+                        str += "O";
+                    }
+
+                    str += "\t";
+                }
+
+                Debug.Log(str);
+            }
+
+            Debug.Log($"\t Matriz C:");
+            for (int i = 0; i < obj.C_OcupiedSpace.GetRows(); i++)
+            {
+                string str = "\t";
+                for (int j = 0; j < obj.C_OcupiedSpace.GetColums(); j++)
+                {
+                    if (obj.C_OcupiedSpace.GetValue(i, j))
+                    {
+                        str += "X";
+                    }
+                    else
+                    {
+                        str += "O";
+                    }
+
+                    str += "\t";
+                }
+
+                Debug.Log(str);
+            }
         }
     }
 }
