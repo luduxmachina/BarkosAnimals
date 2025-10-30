0e77344d1e8d8e5513b161bfd71c09ca7d66aff2
Miguel Lazaro Gil <lazaro.gil.miguel@gmail.com>
2025-10-30 19:36:00 +0100

    Objetos se pasan del carro al barco

--- Assets/_Scripts/ItemInteractionSystem/ItemWithItem/SpecificInteractions/Inventories/CartToBoatHandler.cs
+++ Assets/_Scripts/ItemInteractionSystem/ItemWithItem/SpecificInteractions/Inventories/CartToBoatHandler.cs
@@ -1,20 +1,24 @@
 using UnityEngine;
+using UnityEngine.Events;
+
 [RequireComponent(typeof(Collider))]
 public class CartToBoatHandler : MonoBehaviour
 {
     [SerializeField]
     private ShipData shipData;
+    public UnityEvent onTransferComplete; //ponemos un puf o algo lol
     private void OnTriggerStay(Collider other)
     {
         if (!other.CompareTag("Item")) { return; }
-        CartData cartData = other.GetComponent<CartData>();
+        CartData cartData = other.GetComponentInChildren<CartData>();
         if(cartData == null)
         { return; }
-       // if(cartData.IsEmpty())
-      //  { return; }
+        if (cartData.InventoryIsEmpty())
+        { return; }
 
-        Debug.Log("inventario del carro al barco");
-      //  shipData.TransferInventoryFromCart(cartData);
+        var temp =cartData.ExtractAllInventoryObjects();
+        shipData.TryStackAllItems(temp);
+        onTransferComplete?.Invoke();
 
 
     }
