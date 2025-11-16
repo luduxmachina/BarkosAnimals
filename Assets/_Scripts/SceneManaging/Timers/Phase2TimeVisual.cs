9cb493043a87ee452165495dded70872196cdbd3
Miguel Lazaro Gil <lazaro.gil.miguel@gmail.com>
2025-11-16 21:56:58 +0100

    Update Phase2TimeVisual.cs

--- Assets/_Scripts/SceneManaging/Timers/Phase2TimeVisual.cs
+++ Assets/_Scripts/SceneManaging/Timers/Phase2TimeVisual.cs
@@ -8,5 +8,13 @@ public class Phase2TimeVisual : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI timeText;
+    private void Update()
+    {
+        float elapsedTime = phaseTimer.timeElapsed;
+        int tempE = Mathf.FloorToInt(elapsedTime);
+        float totalTIme= phaseTimer.phaseDuration;
+        int tempT = Mathf.FloorToInt(totalTIme);
+        timeText.text = tempE.ToString() + " m of " + tempT.ToString() + " m";
+    }
 
 }
