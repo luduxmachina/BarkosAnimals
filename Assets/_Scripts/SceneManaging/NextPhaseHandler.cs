22f2f563ba0d71e62d1ae804aa2fc349aa73abfa
MiguelLazaroGil <115834196+MiguelLazaroGil@users.noreply.github.com>
2025-11-24 17:44:16 +0100

    Cambiada seleccion de islas y tal :(

--- Assets/_Scripts/SceneManaging/NextPhaseHandler.cs
+++ Assets/_Scripts/SceneManaging/NextPhaseHandler.cs
@@ -4,30 +4,18 @@ using UnityEngine.Events;
 
 public enum LevelPhases
 {
-    SelectionPhase,
     IslandPhase,
     OrganizationPhase,
     BoatPhase,
-    QuotaPhase
+    QuotaPhase,
+    End
 }
 
-public enum GameModes //esto es criminal y está hecho con spaghetti, todo lo que esté relacionado con esto 
-{
-    SIOBQ,
-    IOSBQ
-}
 [Serializable]
 public class NextPhaseHandler
 {
     [SerializeField]
-    GameModes gameMode;
-    [SerializeField]
     public LevelPhases currentPhase;
-
-    private GameFlowManager gameFlowManager => GameFlowManager.instance;
-
-    [HideInInspector]
-    public UnityEvent onStartSelectionPhase = new();
 
     [HideInInspector]
     public UnityEvent onStartIslandPhase = new();
@@ -37,70 +25,24 @@ public class NextPhaseHandler
 
     [HideInInspector]
     public UnityEvent onStartBoatPhase = new();
-
     [HideInInspector]
     public UnityEvent onStartQuotaPhase = new();
+
     public void Initialize()
     {
 
-        if (gameMode == GameModes.SIOBQ)
-            currentPhase = LevelPhases.SelectionPhase;
-        else
-            currentPhase = LevelPhases.IslandPhase;
+        currentPhase = LevelPhases.IslandPhase;
     }
     public void NextPhase()
     {
+        currentPhase++;
 
-        if (gameMode == GameModes.SIOBQ)
-        {
-            SIOBQNextPhase();
-        }
-        else
-        {
-            IOSBQNextPhase();
-        }
         CallPhaseEvent();
-    }
-    private void IOSBQNextPhase()
-    {
-        switch (currentPhase)
-        {
-            case LevelPhases.IslandPhase:
-                currentPhase = LevelPhases.OrganizationPhase;
-                break;
-            case LevelPhases.OrganizationPhase:
-                currentPhase = LevelPhases.SelectionPhase;
-                break;
-            case LevelPhases.SelectionPhase:
-                currentPhase = LevelPhases.BoatPhase;
-                break;
-            case LevelPhases.BoatPhase:
-                currentPhase = LevelPhases.QuotaPhase;
-                break;
-            case LevelPhases.QuotaPhase:
-                currentPhase = LevelPhases.IslandPhase;
-                break;
-        }
-
-    }
-    private void SIOBQNextPhase()
-    {
-        if (currentPhase == LevelPhases.QuotaPhase)
-        {
-            currentPhase = LevelPhases.SelectionPhase;
-        }
-        else
-        {
-            currentPhase++;
-        }
     }
     void CallPhaseEvent()
     {
         switch (currentPhase)
         {
-            case LevelPhases.SelectionPhase:
-                onStartSelectionPhase.Invoke();
-                break;
             case LevelPhases.IslandPhase:
                 onStartIslandPhase.Invoke();
                 break;
@@ -119,7 +61,7 @@ public class NextPhaseHandler
     {
         return phase switch
         {
-            LevelPhases.SelectionPhase => onStartSelectionPhase,
+
             LevelPhases.IslandPhase => onStartIslandPhase,
             LevelPhases.OrganizationPhase => onStartOrganizationPhase,
             LevelPhases.BoatPhase => onStartBoatPhase,
