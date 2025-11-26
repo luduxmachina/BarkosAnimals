8084dff1ec4d16bd1041c7112e74b14cee90cb4c
MiguelLazaroGil <115834196+MiguelLazaroGil@users.noreply.github.com>
2025-11-26 19:47:02 +0100

    cambios supongo?

--- Assets/_Scripts/Grid/Custom Grid Editor/BoolMatrixDrawer.cs
+++ Assets/_Scripts/Grid/Custom Grid Editor/BoolMatrixDrawer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 
@@ -118,3 +119,4 @@ public class CustomBoolMatrixDrawer : PropertyDrawer
         EditorGUI.EndProperty();
     }
 }
+#endif
