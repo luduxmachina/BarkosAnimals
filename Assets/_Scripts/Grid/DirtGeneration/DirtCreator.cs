e424a9ccfaf23a6715aec848578192632edffd47
FRANCISCO PEÑA CALDITO <125310916+robotronic12@users.noreply.github.com>
2025-11-30 22:24:28 +0100

    La suciedad ahora spawnea sola por el barco

---
+++ Assets/_Scripts/Grid/DirtGeneration/DirtCreator.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using Random = UnityEngine.Random;
+
+public class DirtCreator : MonoBehaviour
+{
+    [SerializeField] 
+    private GameObject dirtPrefab;
+    [SerializeField] 
+    private Transform dirtParent;
+    
+    [SerializeField]
+    private Grid grid;
+
+    [SerializeField] 
+    private int placementTries = 5;
+    [SerializeField] 
+    private float yOffset = 0.1f;
+    [SerializeField]
+    private float cooldown = 5f;
+    [SerializeField] 
+    private float probabilityToSpreadNearOtherDirt = 0.6f;
+    
+    public UnityEvent<Vector2Int> onDirtPlacedGrid = new UnityEvent<Vector2Int>();
+    public UnityEvent<Vector3> onDirtPlacedWorld = new UnityEvent<Vector3>();
+
+    private HashSet<Vector2Int> dirtsPlaced = new HashSet<Vector2Int>();
+    
+    private Vector2Int gridSize = new Vector2Int();
+    private bool startPlacing = false;
+    private float timer = 0f;
+
+    private void Update()
+    {
+        if(!startPlacing)
+            return;
+
+        timer += Time.deltaTime;
+        if(timer > cooldown)
+        {
+            PlaceDirt();
+            timer = 0f;
+        }
+    }
+
+    public void StartPlacingDirt()
+    {
+        SetPlacingDirt(true);
+    }
+
+    public void StopPlacingDirt()
+    {
+        SetPlacingDirt(false);
+    }
+
+    public void SetPlacingDirt(bool placeDirt)
+    {
+        startPlacing =  placeDirt;
+    }
+
+    public void RemoveDirt(Vector3 dirtWorldPosition)
+    {
+        Vector3Int pos3 = grid.WorldToCell(dirtWorldPosition);
+        Vector2Int pos = new Vector2Int(pos3.x, pos3.z);
+
+        if (dirtsPlaced.Contains(pos))
+        {
+            dirtsPlaced.Remove(pos);
+        }
+        else
+        {
+            Debug.LogError($"No dirts placed at {dirtWorldPosition}");
+        }
+    }
+
+    public void ConfigureGrid(CustomBoolMatrix boolMatrix)
+    {
+        gridSize.x = boolMatrix.GetRows();
+        gridSize.y = boolMatrix.GetColums();
+    }
+
+    private void PlaceDirt()
+    {
+        if (gridSize.sqrMagnitude < 0.001f)
+            return;
+        
+        if (dirtsPlaced.Count <= 0) // No dirt in scene
+        {
+            PlaceDirtAlone();
+        }
+        else // Dirt in scene
+        {
+            if (Random.value < probabilityToSpreadNearOtherDirt)
+            {
+                PlaceDirtNextTo();
+            }
+            else
+            {
+                PlaceDirtAlone();
+            }
+        }
+    }
+
+    private void PlaceDirtNextTo()
+    {
+        int n = dirtsPlaced.Count;
+        int randIndex = Random.Range(0, n);
+        Vector2Int lookingAtPos = dirtsPlaced.ToArray()[randIndex];
+
+        Vector2Int gridPos;
+        int i = 0;
+        do
+        {
+            gridPos = new Vector2Int(lookingAtPos.x + Random.Range(-1, 2), lookingAtPos.y + Random.Range(-1, 2));
+            
+            i++;
+            if (i > placementTries)
+                return;
+        } while (dirtsPlaced.Contains(gridPos));
+        
+        PlaceDirtAt(gridPos);
+    }
+
+    private void PlaceDirtAlone()
+    {
+        int rows = gridSize.x;
+        int cols = gridSize.y;
+        
+        int x = 0;
+        int y = 0;
+
+        Vector2Int gridPos;
+        int i = 0;
+        do
+        {
+            x = (int)Random.Range(-rows * 0.5f, rows * 0.5f);
+            y = (int)Random.Range(-cols * 0.5f, cols * 0.5f);
+            gridPos = new Vector2Int(x, y);
+            
+            i++;
+            if(i > placementTries)
+                return;
+            
+        } while (dirtsPlaced.Contains(gridPos));
+
+        PlaceDirtAt(gridPos);
+    }
+
+    private void PlaceDirtAt(Vector2Int gridPos)
+    {
+        dirtsPlaced.Add(gridPos);
+
+        Vector3 pos = grid.GetCellCenterWorld(new Vector3Int(gridPos.x, 0, gridPos.y));
+        pos = new Vector3(pos.x, pos.y + yOffset, pos.z);
+
+        if (dirtPrefab != null)
+        {
+            Instantiate(dirtPrefab, pos, Quaternion.identity, dirtParent);
+        }
+        else
+        {
+            Instantiate(dirtPrefab, pos, Quaternion.identity);
+        }
+        
+        onDirtPlacedGrid?.Invoke(gridPos);
+        onDirtPlacedWorld?.Invoke(pos);
+    }
+}
