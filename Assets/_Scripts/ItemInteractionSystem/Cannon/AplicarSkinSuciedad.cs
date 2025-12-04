39195b16cce4b3436f56d420d438f3cf9f21ddb9
Miguel Lazaro Gil <lazaro.gil.miguel@gmail.com>
2025-12-04 19:31:23 +0100

    ApplySkin de suciedad hecho, falta unirlo con aanimal y meterlo en los prefabs

---
+++ Assets/_Scripts/ItemInteractionSystem/Cannon/AplicarSkinSuciedad.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class AplicarSkinSuciedad : MonoBehaviour
+{
+    [SerializeField]
+    AAnimalFase2 animalFase2;
+    [SerializeField]
+    Renderer rendererAnimal;
+    [SerializeField]
+    float empieceAplicarSuciedad = 0.2f;
+    [SerializeField]
+    float suciedadFinal = 1f;
+    [SerializeField]
+    float suciedadInicio = 0.0f;
+    float lastAppliedSuciedad = 0f;
+    void Update()
+    {
+        float sucideadNormalizada = 0.5f;
+        if(sucideadNormalizada < empieceAplicarSuciedad)
+        {
+            if(lastAppliedSuciedad != 0f)
+            {
+                lastAppliedSuciedad = 0f;
+                rendererAnimal.material.SetFloat("Suciedad", 0);
+            }
+        }
+        sucideadNormalizada = Mathf.Clamp(sucideadNormalizada, suciedadInicio, suciedadFinal);
+
+        rendererAnimal.material.SetFloat("Suciedad", sucideadNormalizada);
+        lastAppliedSuciedad = sucideadNormalizada;
+    }
+}
