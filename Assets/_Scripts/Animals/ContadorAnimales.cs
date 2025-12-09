df8add197471ff4a70e39f5020e41a42e0f754e7
Alba Poza Vela <125303353+Albi52@users.noreply.github.com>
2025-12-09 20:21:25 +0100

    Contador animales actualizado

--- Assets/_Scripts/Animals/ContadorAnimales.cs
+++ Assets/_Scripts/Animals/ContadorAnimales.cs
@@ -1,9 +1,13 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ContadorAnimales : MonoBehaviour
 {
+
+    [SerializeField]UnityEvent OnNoAnimals;
+
     public void checkCuoteWithOnlySecondFase()
     {
         GameFlowManager.instance.quotaChecker.ResetQuote();
@@ -23,5 +27,22 @@ public class ContadorAnimales : MonoBehaviour
         }
 
 
+    }
+
+    public void Update()
+    {
+        List<Stable> stables = Stable.allStables;
+
+        int numAnim = 0;
+
+        foreach (Stable stable in stables)
+        {
+            numAnim += stable.GetAnimalsInEstable();
+        }
+
+        if(numAnim<0)
+        {
+
+        }
     }
 }
